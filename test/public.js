ae1fafad957bd95f5dd6a5a6546ed4c0b5815f33
Eric Dauenhauer <eric_yd@yahoo.com>
2017-03-05 22:32:04 -0800

    add future public function tests

---
+++ test/public.js
@@ -0,0 +1,21 @@
+var assert = require('chai').assert;
+
+// TODO: add these
+describe.skip('Public functions', function () {
+    describe('exponentialBackoff', function () {
+        it('should wait exponentially longer between each call', function () {
+
+        });
+
+        it('should stop trying after 5 attempts', function () {
+
+        });
+
+        it('should set a trigger to re-start if it fails 5 times', function () {
+
+        });
+    })
+
+    // TODO: are there ways to refactor other public functions to be testable?
+    // In particular, consider ways that I could pass stubs instead of making real API calls
+})
